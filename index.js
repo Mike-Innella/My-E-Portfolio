c2687b00abc19d2f60a3d72ff8b746083056f81f
Michael Innella <mainnella@gmail.com>
2024-10-19 19:27:51 -0400

    finished most modal, fine tuning it for smoother behavior.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 //service_mygmail
 //cePFoU8dvsaDAlAyz
 
-const loading = document.querySelector(".modal__overlay--loading");
-const success = document.querySelector(".modal__overlay--success");
-
 function contact(event) {
   event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
   emailjs
     .sendForm(
       "service_mygmail",
@@ -24,9 +24,17 @@ function contact(event) {
           "The email service is temporarily unavailable. Apologies, please contact me directly at mainnella@gmail.com."
         );
       });
-
       loading.classList += " modal__overlay--visible";
-
       console.log("it worked");
     }, 1000);
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
